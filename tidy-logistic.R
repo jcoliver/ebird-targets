98aab798a59c1a71934b5f3357d9d69063edd802
Jeff Oliver <jcoliver@email.arizona.edu>
2021-04-19 15:51:42 -0700

    Add empty file for tidymodels approach

---
+++ tidy-logistic.R
@@ -0,0 +1,8 @@
+# Use tidymodels approach for logistic regression model selection
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2021-04-19
+
+# See materials on lessons 10 & 11
+# 10 https://www.tmwr.org/resampling.html
+# 11 https://www.tmwr.org/compare.html
